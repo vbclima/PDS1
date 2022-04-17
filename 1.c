1daa44cf03307de620fb364f5278e62a8d9e5dfa
Vitor Lima <103759032+vbclima@users.noreply.github.com>
2022-04-17 12:56:15 -0300

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (){
+	
+	printf ("Alo, mundo!!!!");
+
+	return 0;
+}
---
+++ 3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+void main (){
+	
+	float a;
+	float b;
+	
+	a = sin(3.14);
+	b = sin(4.13);
+	
+	printf("O seno de 3,14 eh: %f\nO seno de 4,13 eh: %f", a, b);
+	
+	
+}

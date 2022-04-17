2e3403a1a96850df0b13c33d3687a43d4e3f8622
Vitor Lima <103759032+vbclima@users.noreply.github.com>
2022-04-17 12:58:07 -0300

    Add files via upload

--- 1.c
+++ 1.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main (){
+void main (){
 	
-	printf ("Alo, mundo!!!!");
-
-	return 0;
+	char v, i, t, o, r;
+	
+	v='V';
+	i='i';
+	t='t';
+	o='o';
+	r='r';
+	
+	printf ("O coodigo ascii do meu nome eh: %d %d %d %d %d", v, i, t, o, r);
+	
 }
